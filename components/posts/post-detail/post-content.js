09edd02492957b471e3aa15f889565ceb0c8a09d
Eder <chlrobert93@gmail.com>
2021-10-06 15:59:59 -0500

    Representación de Markdown como JSX

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,3 +1,5 @@
+import ReactMarkdown from 'react-markdown';
+
 import PostHeader from './post-header';
 import classes from './post-content.module.css';
 
@@ -6,7 +8,7 @@ const DUMMY_POST = {
   title: 'Empezando con NextJS',
   image: 'Empezando-NextJS.png',
   date: '2022-02-10',
-  content: '# This is a first post',
+  content: '# Este es un primer post',
 };
 
 
@@ -16,10 +18,12 @@ function PostContent() {
     
   const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
 
+  console.log(imagePath);
+
   return (
     <article className={classes.content}>
       <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      {DUMMY_POST.content}
+      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
     </article>
   );
 }
