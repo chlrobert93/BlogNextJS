78a92bfba2cd120d21f7c7fb1d294c5a135c9b6e
Eder <chlrobert93@gmail.com>
2021-10-11 18:56:57 -0500

    Procesamiento de imágenes con el componenete from Markdown

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,9 +1,11 @@
 
 import React from 'react';
-import ReactMarkdown from 'react-markdown';
+import ReactMarkdown from 'react-markdown'
+import Image from 'next/image';
 
 import PostHeader from './post-header';
 import classes from './post-content.module.css';
+
 /* 
 const DUMMY_POST = {
   slug: 'empezando-con-nextjs1',
@@ -19,17 +21,57 @@ const DUMMY_POST = {
 function PostContent(props) {
    
   const { post } = props;
+ // console.log(post.slug);
   
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
+ 
 
   /* const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`; */
+  //markdown llamará a este método
+  //este método, obtenemos datos sobre la imagen que se encontro
+  const customRenderers = {
+    /* img(image) {
+      return (
+        <Image
+          src={`/images/posts/${post.slug}/${image.src}`}
+          alt={image.alt}
+          width={600}
+          height={300}
+        />
+      );
+    }, */
+    //Revisar los parrafos
+
+    p(paragraph) {
+      //extraer el nodo real que sería renderizado por react-markdown
+      const { node } = paragraph;
+      //Solo renderizar la imgagens
+
+      if (node.children[0].tagName === 'img') {
+        const image = node.children[0];
+
+        return(
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        )
+      }
+      return <p>{paragraph.children}</p>;
+    },
+  };
 
 
+ 
 
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 }
--- posts/getting-started-with-nextjs.md
+++ posts/getting-started-with-nextjs.md
@@ -1,14 +1,34 @@
 ---
-title: 'empezando-con-nextjs'
+title: 'Empezando-con-nextjs'
 date: '2022-10-15'
 image: 'getting-started-nextjs.png'
 excerpt: 'NextJS es un framework de React para la producción: hace que la construcción de aplicaciones y sitios React completos sea muy fácil.'
 isFeactured: true
-
 ---
 
+NextJS es un ** marco para ReactJS ** .
+
+Espera un segundo ... ¿un "marco" para React? ¿No es React en sí mismo un marco para JavaScript?
+
+Bueno ... en primer lugar, React es una "biblioteca" para JavaScript. Eso parece ser importante para algunas personas.
+
+No para mí, pero aún así, hay un punto válido: React ya es un marco / biblioteca para JavaScript. Entonces ya es una capa adicional sobre JS.
+
+##  ¿Por qué entonces necesitaríamos NextJS?
+
+Porque NextJS facilita la creación de aplicaciones React, especialmente las aplicaciones React que deberían tener renderizado del lado del servidor (aunque hace mucho más que simplemente ocuparse de eso).
+
+En este artículo, profundizaremos en los conceptos y características centrales que NextJS tiene para ofrecer:
+
+- Enrutamiento basado en archivos
+- Representación previa de página incorporada
+- Capacidades de obtención de datos enriquecidos
+- Optimización de imagen
+- Mucho más
 
 
-#This is a title
+## File-based Routing
 
-This is some text with a [link](https://gogle.com)
+![Create routes via your file + folder structure](nextjs-file-based-routing.png)
+
+... Más contenido ...
