4851d0c447cfae234fba0268f056acd6ca327443
Eder <chlrobert93@gmail.com>
2021-10-12 19:17:40 -0500

    Solucionar error lib

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -4,7 +4,7 @@ import ReactMarkdown from 'react-markdown';
 
 import PostHeader from './post-header';
 import classes from './post-content.module.css';
-
+/* 
 const DUMMY_POST = {
   slug: 'empezando-con-nextjs1',
   title: 'Empezando con NextJS',
@@ -12,20 +12,24 @@ const DUMMY_POST = {
   date: '2022-02-10',
   content: '# Este es un primer post',
 };
+ */
 
 
 
+function PostContent(props) {
+   
+  const { post } = props;
+  
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
-function PostContent() {
-    
-  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+  /* const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`; */
 
-  console.log(imagePath);
+
 
   return (
     <article className={classes.content}>
-      <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown>{post.content}</ReactMarkdown>
     </article>
   );
 }
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,8 +1,38 @@
 import PostContent from '../../components/posts/post-detail/post-content';
+import { getPostData, getPostsFiles } from '../../lib/posts-util';
 
-function PostDetailPage() {
-  return  <PostContent />
+function PostDetailPage(props) {
+  return  <PostContent post={props.posts} />
      
+}
+
+export function getStaticProps(context){
+  const { params } = context;
+  const { slug } = params;
+
+  const postData = getPostData(slug);
+
+  return{
+    props: {
+      posts: postData
+    },
+
+    revalidate: 600
+  };
+}
+
+//Obtener rutas estáticas para avisar a NextJS qué valores deneríamos pregenerar
+export function getStaticPaths(){
+
+  //Todos los nombres de archivos de las publicaciones
+  const postFilenames =  getPostsFiles();
+
+  const slugs = postFilenames.map((fileName) => fileName.replace(/\.md$/,''));
+
+  return{
+    paths: slugs.map(slug => ({ params: {slug : slug}})),
+    fallback: false
+  };
 }
 
 export default PostDetailPage;
