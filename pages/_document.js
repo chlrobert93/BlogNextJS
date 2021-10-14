19be7687091cd095e2207e554f8c8ed0860851fd
Eder <chlrobert93@gmail.com>
2021-10-14 17:41:12 -0500

    Agregando un archivo _document.js

---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang='es'>
+        <Head />
+        <body>
+          <div id='overlays' />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
+  
+
